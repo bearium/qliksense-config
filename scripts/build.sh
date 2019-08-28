04f7f9e3279ea753cd7c084eb25c42e0db1e2f70
bearium <jacob_martin@rogers.com>
2019-08-28 11:31:14 -0400

    fix make file script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@ KUSTOMIZE_DIR="$(pwd)/kustomize"
 DEST_DIR=$HOME/.config/kustomize
 
 cd $KUSTOMIZE_DIR
-find . -iname '*.go' -exec sh -c 'f="{}"; GO111MODULE=on go build -buildmode plugin -o "${f/go/so}" $f' \;
+find . -iname '*.go' -exec sh -c 'f="{}"; GO111MODULE=on go build -buildmode plugin -o "${f/.go/.so}" $f' \;
 if [ ! -z "${XDG_CONFIG_HOME}" ]; then
 DEST_DIR=$XDG_CONFIG_HOME
 fi
